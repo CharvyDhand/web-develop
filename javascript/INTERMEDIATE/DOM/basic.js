eb4beda8de8f9bbf9c82280d326c5c23d3e32c0f
CharvyDhand <charvydhand13@gmail.com>
2025-02-21 22:24:44 +0530

    update

---
+++ javascript/INTERMEDIATE/DOM/basic.js
@@ -0,0 +1,8 @@
+//console.log(window) on console
+//console.log(window.document)  
+//console.dir(document)
+//dom is document object model it manipulates HTML
+// OBJECT IS h1,div,body
+//DOM represents a document as a logical tree of nodes and objects 
+// Each node contains objects 
+// DOM methods allow programs to access the tree and change the document's structure, style, or content 
