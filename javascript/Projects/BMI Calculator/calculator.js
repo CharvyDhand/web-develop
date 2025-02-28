5c84cde90454a0e71b53a177ef66401183be755c
CharvyDhand <charvydhand13@gmail.com>
2025-02-28 23:50:43 +0530

    update

--- javascript/Projects/BMI Calculator/calculator.js
+++ javascript/Projects/BMI Calculator/calculator.js
@@ -4,15 +4,16 @@ forms.addEventListener('submit',function(e){
     e.preventDefault()
     const h=parseInt(document.querySelector('#height').value)
     const w=parseInt(document.querySelector('#weight').value)   //we cant take input outside the scope then that will give you empty value 
-    const result=parseInt(document.querySelector('#result').value) 
+    console.log(h,w)
+    const results=document.querySelector('#result') 
     if(h===' '|| h<0 ||isNaN(h)){
-        result.innerHTML='please give valid input'
+        results.innerHTML='please give valid input'
     }
     else if(w===' '|| w<0 ||isNaN(w)){
-        result.innerHTML='please give valid input'
+        results.innerHTML='please give valid input'
     }
     else{
-     const r=(w/((h*h)/10000)).toFixed(2)
-     result.innerHTML='<span>${bmi}</span>'
+     const bmi=(w/((h*h)/10000)).toFixed(2)
+     results.innerHTML=`<span>${bmi}</span>`
     }
 })
