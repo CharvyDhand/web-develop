2a6d677cc8e2707abc0f3e442e4535d527cab323
CharvyDhand <charvydhand13@gmail.com>
2025-03-16 23:09:07 +0530

    update

---
+++ javascript/Advance/object oriented/mathpi.js
@@ -0,0 +1,27 @@
+console.log(Math.PI)
+Math.PI=40
+console.log(Math.PI)  //we cannot override
+const d=Object.getOwnPropertyDescriptor(Math,'PI')
+console.log(d)
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai));
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: false 
+})
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
